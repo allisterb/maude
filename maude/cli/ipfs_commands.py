bf86a37cc4b904e30aa6665bfad77b3d4e7fa727
Allister Beharry <allister.beharry@gmail.com>
2022-03-27 15:20:18 -0400

    Fix bug in context handler.

--- maude/cli/ipfs_commands.py
+++ maude/cli/ipfs_commands.py
@@ -9,13 +9,9 @@ from core import ipfs
 
 def init_ipfs_client(api_url=None):
     with begin("Connecting to IPFS node") as op:
-        try:
-            ipfs.ipfsclient = ipfshttpclient.connect(session=True)
-            op.complete()
-        except ConnectionError as ce:
-            op.abandon()
-            exit_with_error(f'Error connecting to IPFS API: {ce}')
-
+        ipfs.ipfsclient = ipfshttpclient.connect(session=True)
+        op.complete()
+        
 @ipfscmd.command('info')
 def ipfs_info():
     init_ipfs_client()
