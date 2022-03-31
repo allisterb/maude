4f2799cf55ddb4f9d315b579a08142c545df457a
Allister Beharry <allister.beharry@gmail.com>
2022-03-30 22:39:15 -0400

    Add compile_rules command.

--- maude/binary/yara_classifier.py
+++ maude/binary/yara_classifier.py
@@ -1,11 +1,44 @@
+# Contains code from https://github.com/airbnb/binaryalert/blob/master/rules/compile_rules.py
+
 import os
 from logging import info, error
+from typing import Generator
 
 import yara
 
 import maude_global
 from base.timer import begin
 from core.binary_classifier import BinaryClassifier
+
+def _find_yara_files(rules_dir) -> Generator[str, None, None]:
+    """Find all .yar[a] files in the rules directory.
+
+    Yields:
+        YARA rule filepaths, relative to the rules root directory.
+    """
+    for root, _, files in os.walk(rules_dir):
+        for filename in files:
+            lower_filename = filename.lower()
+            if lower_filename.endswith('.yar') or lower_filename.endswith('.yara'):
+                yield os.path.relpath(os.path.join(root, filename), start=rules_dir)
+
+
+def compile_rules(rules_dir, target_path: str) -> None:
+    """Compile YARA rules into a single binary rules file.
+
+    Args:
+        target_path: Where to save the compiled rules file.
+    """
+    # Each rule file must be keyed by an identifying "namespace"; in our case the relative path.
+    yara_filepaths = {relative_path: os.path.join(rules_dir, relative_path)
+                    for relative_path in _find_yara_files(rules_dir)}
+
+    # Compile all available YARA rules. Note that external variables are defined but not set;
+    # these will be set at runtime by the lambda function during rule matching.
+    rules = yara.compile(
+        filepaths=yara_filepaths,
+        externals={'extension': '', 'filename': '', 'filepath': '', 'filetype': ''})
+    rules.save(target_path)
 
 class Classifier(BinaryClassifier):
     """YARA binary classiifier"""
@@ -20,5 +53,4 @@ class Classifier(BinaryClassifier):
         with begin(f'Classifying {filename} using NudeNet') as op:
             matches = self.rules.match(filename)
             op.complete()
-            return matches 
-        
+            return matches
--- maude/cli/binary_commands.py
+++ maude/cli/binary_commands.py
@@ -4,13 +4,19 @@ from logging import info, error
 import click
 from rich import print
 
+from base.timer import begin
 from cli.commands import binary
 from cli.util import exit_with_error
 
-@binary.command('yara', help='Run YARA rules on an executable.')
-@click.option('--def', 'ruleset', flag_value='nsfw', default=True,  help='Use the nsfw model: https://github.com/GantMan/nsfw_model.')
-@click.argument('filename', type=click.Path(exists=True))
-def yara(ruleset, filename):
-    from binary.yara_classifier import Classifier
-    c = Classifier()
+@binary.group('yara', help='Run YARA rules on an executable.')
+def yaracmd():
+    pass
     
+@yaracmd.command('compile', help='Compile YARA rules in a directory into a single file.')
+@click.argument('rules-dir', type=click.Path(exists=True))
+@click.argument('output-file', type=click.Path(exists=False))
+def compile(rules_dir, output_file):
+    from binary.yara_classifier import compile_rules
+    with begin(f'Compiling YARA rules in {rules_dir} to {output_file}') as op:
+        compile_rules(rules_dir, output_file)
+        op.complete()
